0d2831d4791c33f3a710dbb0d0181a73bc7685a5
Daniel Piron <dannypiron@gmail.com>
2021-10-08 20:21:50 -0400

    Load .obj as triangle list

--- src/load_obj.h
+++ src/load_obj.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <map>
 #include <sstream>
 #include <string>
@@ -35,10 +36,54 @@ struct ObjFile {
         }
     };
 
+    struct TriangleCollector {
+        virtual void handle_vertex(const Vertex&, const TextureCoordinates&,
+                                   const VertexNormal&) = 0;
+        virtual ~TriangleCollector() {}
+    };
+
+    struct Face {
+        struct Indices {
+            int vertex;
+            int texture;
+            int normal;
+
+            Indices(int vertex_index, int texture_index, int normal_index)
+                : vertex(vertex_index), texture(texture_index), normal(normal_index)
+            {
+            }
+
+            Indices(const std::vector<int>& v)
+            {
+                if (v.size() >= 1) {
+                    vertex = v[0];
+                }
+                if (v.size() >= 2) {
+                    texture = v[1];
+                }
+                if (v.size() >= 3) {
+                    normal = v[2];
+                }
+            }
+
+            bool operator==(const Indices& other) const
+            {
+                return vertex == other.vertex && texture == other.texture && normal == other.normal;
+            }
+        };
+        std::vector<Indices> indices;
+        Face() {}
+        Face(const Face&) = default;
+        Face(std::initializer_list<Indices> il) : indices(il) {}
+
+        bool operator==(const Face& other) const { return indices == other.indices; }
+    };
+
     struct Object {
         std::vector<Vertex> vertices;
         std::vector<TextureCoordinates> tex_coords;
         std::vector<VertexNormal> vertex_normals;
+        std::vector<Face> faces;
     };
 
     static LinePartition partition_line(const std::string& line)
@@ -63,6 +108,18 @@ struct ObjFile {
             return "";
         }
         return s.substr(first_non_whitepace, last_non_whitespace - first_non_whitepace + 1);
+    }
+
+    static std::vector<std::string> split(const std::string& s, char delimiter)
+    {
+        std::stringstream ss(s);
+        std::string element;
+
+        std::vector<std::string> result;
+        while (std::getline(ss, element, delimiter)) {
+            result.emplace_back(element);
+        }
+        return result;
     }
 
     static Vertex parse_vertex(const std::string& s)
@@ -109,15 +166,18 @@ struct ObjFile {
         return {x, y, z};
     }
 
-    static std::vector<std::string> split(const std::string& s, char delimiter)
+    static Face parse_face(const std::string& s)
     {
-        std::stringstream ss(s);
-        std::string element;
-
-        std::vector<std::string> result;
-        while (std::getline(ss, element, delimiter)) {
-            result.emplace_back(element);
+        Face result;
+        const auto vertex_data = split(s, ' ');
+        for (const auto& vertex_text : vertex_data) {
+            const auto raw_indices = split(vertex_text, '/');
+            std::vector<int> indices;
+            std::transform(raw_indices.begin(), raw_indices.end(), std::back_inserter(indices),
+                           [](const auto& s) { return std::stoi(s); });
+            result.indices.emplace_back(indices);
         }
+
         return result;
     }
 
@@ -126,7 +186,6 @@ struct ObjFile {
     void process_line(const std::string& s)
     {
         const auto line = partition_line(strip_whitespace(strip_comments(s)));
-        std::cout << "Line " << line.command << std::endl;
         if (line.command == "o") {
             const auto object_name = line.parameters;
             _object_names.push_back(object_name);
@@ -140,6 +199,8 @@ struct ObjFile {
         } else if (line.command == "vn") {
             _objects[_current_object].vertex_normals.push_back(
                 parse_vertex_normal(line.parameters));
+        } else if (line.command == "f") {
+            _objects[_current_object].faces.push_back(parse_face(line.parameters));
         }
     }
 
@@ -152,6 +213,23 @@ struct ObjFile {
 
     int object_count() const { return static_cast<int>(_object_names.size()); }
     const std::vector<std::string>& objects() const { return _object_names; }
+
+    void produce_triangle_list(const std::string& object_name, TriangleCollector* collector)
+    {
+        const Object& obj = _objects.at(object_name);
+        for (const auto& face : obj.faces) {
+            // grab only the first 3 vertices of each face. If output was not trianglated, we will
+            // have some "holes", but at least our output will be useable.
+            for (size_t i = 0; i < 3; ++i) {
+                const auto v_index = static_cast<size_t>(face.indices[i].vertex - 1);
+                const auto t_index = static_cast<size_t>(face.indices[i].texture - 1);
+                const auto n_index = static_cast<size_t>(face.indices[i].normal - 1);
+                collector->handle_vertex(obj.vertices[v_index], obj.tex_coords[t_index],
+                                         obj.vertex_normals[n_index]);
+            }
+        }
+    }
+
     std::vector<std::string> _object_names;
     std::map<std::string, Object> _objects;
     std::string _current_object;
--- tests/test_objFileLoader.cpp
+++ tests/test_objFileLoader.cpp
@@ -2,6 +2,101 @@
 
 #include <load_obj.h>
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <ostream>
+
+static const char* blender_output =
+    R"(
+# Blender v2.93.4 OBJ File: ''
+# www.blender.org
+mtllib cube.mtl
+o Cube
+v 1.000000 1.000000 -1.000000
+v 1.000000 -1.000000 -1.000000
+v 1.000000 1.000000 1.000000
+v 1.000000 -1.000000 1.000000
+v -1.000000 1.000000 -1.000000
+v -1.000000 -1.000000 -1.000000
+v -1.000000 1.000000 1.000000
+v -1.000000 -1.000000 1.000000
+vt 0.875000 0.500000
+vt 0.625000 0.750000
+vt 0.625000 0.500000
+vt 0.375000 1.000000
+vt 0.375000 0.750000
+vt 0.625000 0.000000
+vt 0.375000 0.250000
+vt 0.375000 0.000000
+vt 0.375000 0.500000
+vt 0.125000 0.750000
+vt 0.125000 0.500000
+vt 0.625000 0.250000
+vt 0.875000 0.750000
+vt 0.625000 1.000000
+vn 0.0000 1.0000 0.0000
+vn 0.0000 0.0000 1.0000
+vn -1.0000 0.0000 0.0000
+vn 0.0000 -1.0000 0.0000
+vn 1.0000 0.0000 0.0000
+vn 0.0000 0.0000 -1.0000
+usemtl Material
+s off
+f 5/1/1 3/2/1 1/3/1
+f 3/2/2 8/4/2 4/5/2
+f 7/6/3 6/7/3 8/8/3
+f 2/9/4 8/10/4 6/11/4
+f 1/3/5 4/5/5 2/9/5
+f 5/12/6 2/9/6 6/7/6
+f 5/1/1 7/13/1 3/2/1
+f 3/2/2 7/14/2 8/4/2
+f 7/6/3 5/12/3 6/7/3
+f 2/9/4 4/5/4 8/10/4
+f 1/3/5 3/2/5 4/5/5
+f 5/12/6 1/3/6 2/9/6
+)";
+
+// 6/11/4
+// v -1.000000 -1.000000 -1.000000
+// vt 0.125000 0.500000
+// vn 0.0000 -1.0000 0.0000
+
+std::ostream& operator<<(std::ostream& os, const ObjFile::Face::Indices& i)
+{
+    os << i.vertex << '/';
+    os << i.texture << '/';
+    os << i.normal;
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const ObjFile::Face& f)
+{
+    for (const auto& indice : f.indices) {
+        os << indice << " ";
+    }
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const glm::vec2& v)
+{
+    os << glm::to_string(v);
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const glm::vec3& v)
+{
+    os << glm::to_string(v);
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const glm::vec4& v)
+{
+    os << glm::to_string(v);
+    return os;
+}
+
 TEST(ObjFileLineParition, CanPartitionWithSingleSpace)
 {
     const auto result = ObjFile::partition_line("cmd remaining text");
@@ -96,4 +191,57 @@ TEST(objFileLoader, CanReadVertexNormals)
     ASSERT_EQ(obj.object_count(), 1);
     ASSERT_EQ(obj.objects()[0], "Cube");
     EXPECT_EQ(obj["Cube"].vertex_normals, expected);
+}
+
+TEST(objFileLoader, CanReadFaces)
+{
+    auto text = R"(o Cube
+                   f 1/1/1 2/2/2 3/3/3
+                   )";
+    ObjFile obj;
+    obj.process_text(text);
+
+    ObjFile::Face expected{ObjFile::Face::Indices{1, 1, 1}, ObjFile::Face::Indices{2, 2, 2},
+                           ObjFile::Face::Indices{3, 3, 3}};
+    ASSERT_EQ(obj.object_count(), 1);
+    ASSERT_EQ(obj.objects()[0], "Cube");
+    ASSERT_EQ(obj["Cube"].faces.size(), 1);
+    EXPECT_EQ(obj["Cube"].faces[0], expected);
+}
+
+TEST(objFileLoader, CanProductTriangleList)
+{
+    struct TestCollector : public ObjFile::TriangleCollector {
+        struct Vertex {
+            glm::vec4 v;
+            glm::vec2 t;
+            glm::vec3 n;
+        };
+
+        void handle_vertex(const ObjFile::Vertex& vert, const ObjFile::TextureCoordinates& tex,
+                           const ObjFile::VertexNormal& norm) override
+        {
+            vertices.push_back(
+                {{vert.x, vert.y, vert.z, vert.w}, {tex.u, tex.v}, {norm.x, norm.y, norm.z}});
+        }
+
+        std::vector<Vertex> vertices;
+    };
+
+    ObjFile obj;
+    obj.process_text(blender_output);
+
+    TestCollector collector;
+    obj.produce_triangle_list("Cube", &collector);
+
+    ASSERT_EQ(collector.vertices.size(), 36);
+    // Vertex #1
+    EXPECT_EQ(collector.vertices[0].v, glm::vec4(-1.000000f, 1.000000f, -1.000000f, 1.f));
+    EXPECT_EQ(collector.vertices[0].t, glm::vec2(0.875000f, 0.500000f));
+    EXPECT_EQ(collector.vertices[0].n, glm::vec3(0, 1.f, 0));
+
+    // Vertex #12
+    EXPECT_EQ(collector.vertices[11].v, glm::vec4(-1.000000f, -1.000000f, -1.000000f, 1.f));
+    EXPECT_EQ(collector.vertices[11].t, glm::vec2(0.125000f, 0.500000f));
+    EXPECT_EQ(collector.vertices[11].n, glm::vec3(0, -1.f, 0));
 }
