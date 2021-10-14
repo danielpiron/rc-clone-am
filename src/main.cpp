70667fed8f64dbeb7934264a7fd7af259d8e5ab5
Daniel Piron <dannypiron@gmail.com>
2021-10-14 00:22:42 -0400

    Introduce some camera dynamics

--- src/main.cpp
+++ src/main.cpp
@@ -368,6 +368,9 @@ int main(void)
     glVertexAttribPointer(static_cast<GLuint>(vtex_location), 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
                           (void*)offsetof(Vertex, tex));
 
+    glm::vec2 camera_velocity{0};
+    glm::vec2 camera_target = truck.position;
+
     while (!glfwWindowShouldClose(window)) {
         int width, height;
         glfwGetFramebufferSize(window, &width, &height);
@@ -379,12 +382,17 @@ int main(void)
         glClearColor(0.33f, 0.72f, 0.36f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        auto moving_target = truck.position + (truck_state.velocity * 12.0f);
+        auto vector_to_truck = (moving_target - camera_target);
+        float distance_to_camera_target = glm::length(vector_to_truck);
+        camera_velocity = vector_to_truck * .15f;
+        camera_target += camera_velocity;
+
         glm::mat4 view{1.0f};
-        view = glm::translate(view, glm::vec3(0, 0, -40.0f));
+        view = glm::translate(view, glm::vec3(0, 0, -(30.0f + distance_to_camera_target * 2.0f)));
         view = glm::rotate(view, glm::radians(35.264f), glm::vec3(1.0f, 0, 0));
         view = glm::rotate(view, glm::radians(-45.0f), glm::vec3(0, 1.0f, 0));
-
-        view = glm::translate(view, glm::vec3(-truck.position.x, 0, -truck.position.y));
+        view = glm::translate(view, glm::vec3(-camera_target.x, 0, -camera_target.y));
 
         glm::mat4 track_model = glm::mat4{1.0};
         glm::mat4 projection = glm::perspective(glm::radians(35.f), ratio, 0.1f, 100.0f);
